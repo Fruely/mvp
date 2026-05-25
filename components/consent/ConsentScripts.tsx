05c699343e9c82d592ed386a2c2e6c7203bd7fb7
Arthur Niskubin <arthur@example.com>
2026-05-25 22:53:53 +0200

    chore: trigger vercel redeploy

--- components/consent/ConsentScripts.tsx
+++ components/consent/ConsentScripts.tsx
@@ -38,6 +38,12 @@ function loadGoogleAnalytics() {
   window.gtag("js", new Date());
   window.gtag("config", GA_MEASUREMENT_ID, {
     anonymize_ip: true,
+    send_page_view: false,
+  });
+  window.gtag("event", "page_view", {
+    page_title: document.title,
+    page_location: window.location.href,
+    page_path: window.location.pathname,
   });
 }
 
